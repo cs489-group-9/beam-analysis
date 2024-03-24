4ebc47a20b49b7dd0d78586ae0d03dbbde6cdc73
Faizaan Madhani <fzmadhani@gmail.com>
2024-03-24 17:52:32 -0400

    fix bugs in fetch_data script:

--- merge-queues/fetch_data.py
+++ merge-queues/fetch_data.py
@@ -23,11 +23,13 @@ def upload_to_storage(bucket_name: str, file_name: str, data: List[str]):
 def fetch_and_upload_commits(api: GithubAPI):
     logger.info("Fetching commits...")
     total_commits = []
-    for i in tqdm(range(1, 2)):
-        commits = api.fetchData(f"commits?per_page=2&page={i}")
+    for i in tqdm(range(1, 11)):
+        commits = api.fetchData(
+            f"commits?per_page=100&page={i}&until=2024-03-24T00:00:00Z"
+        )
         total_commits.extend(commits)
 
-    commit_shas = set([commit["sha"] for commit in commits])
+    commit_shas = set([commit["sha"] for commit in total_commits])
     upload_to_storage("beam-analysis", "commits.json", total_commits)
 
     return commit_shas
@@ -41,8 +43,8 @@ def fetch_and_upload_push_and_schedule_workflow_runs(
     for sha in tqdm(commit_shas):
         push_runs = api.fetchData(f"actions/runs?event=push&head_sha={sha}")
         schedule_runs = api.fetchData(f"actions/runs?event=schedule&head_sha={sha}")
-        workflow_runs.extend(push_runs)
-        workflow_runs.extend(schedule_runs)
+        workflow_runs.append(push_runs)
+        workflow_runs.append(schedule_runs)
 
     upload_to_storage("beam-analysis", "workflow_runs_push.json", workflow_runs)
 
@@ -65,18 +67,13 @@ def fetch_and_upload_pull_request_and_pull_request_target_workflow_runs(
         "Fetching and uploading pull request and pull request target workflow runs..."
     )
     workflow_runs = []
-    print(pull_request_head_shas)
     for sha in tqdm(pull_request_head_shas):
         pr_runs = api.fetchData(f"actions/runs?event=pull_request&head_sha={sha}")
-        print(pr_runs)
         pr_target_runs = api.fetchData(
             f"actions/runs?event=pull_request_target&head_sha={sha}"
         )
-        print(pr_target_runs)
         workflow_runs.append(pr_runs)
         workflow_runs.append(pr_target_runs)
-
-    print(workflow_runs)
 
     upload_to_storage(
         "beam-analysis", "workflow_runs_pull_requests.json", workflow_runs
